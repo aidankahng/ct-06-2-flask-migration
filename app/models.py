614c62825becb77b31862da28721568aa1444e8d
aidankahng <aidankahng@gmail.com>
2024-03-26 15:54:44 -0700

    model Task now auto saves to db

--- app/models.py
+++ app/models.py
@@ -8,3 +8,12 @@ class Task(db.Model):
     completed = db.Column(db.Boolean, nullable=False, default=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.save()
+    
+    # When creating a new Task, it will automagically be added to our database
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
